08f4280299f0dfa46ac1add0264cfd2b9c9e6902
Blookers <32640219+Blookerss@users.noreply.github.com>
2021-08-29 06:17:33 +0000

    the uh themhmermg

--- src-tauri/src/js/index.js
+++ src-tauri/src/js/index.js
@@ -19,7 +19,7 @@ let languages = [
 const Tauri = window.__TAURI__;
 let currentInput, currentOutput;
 $(window).on("load", async() => {
-    await ky.get("https://goggletrans.blookers.repl.co/api");
+    ky.get("https://goggletrans.blookers.repl.co/api");
     const embedButton = $("#embedButton");
     const transOutput = $(".trans-output");
     const transButton = $("#translateButton");
@@ -82,6 +82,7 @@ $(window).on("load", async() => {
     });
 
     if(Tauri) {
+        $(".download-btn").remove();
         $("#min-win").on('click', (event) => {
             Tauri.window.getCurrent().minimize();
         });
@@ -97,5 +98,93 @@ $(window).on("load", async() => {
         });
     } else {
         $(".win-btn").remove();
+        $("header").children("div:first-child").css(
+            "position",
+            "absolute"
+        ).css(
+            "left",
+            "24px"   
+        );
+        $(".download-btn").css(
+            "position",
+            "absolute"
+        ).css(
+            "right",
+            "24px"
+        );
+    }
+});
+$(".download-btn").on('click', async() => {
+    let nav = $("header");
+    if(!Tauri) {
+        window.location.href = "https://goggletrans.blookers.repl.co/";
+        $(".download-btn").remove();
+        nav.find("*").css(
+            "transition",
+            "all 500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+        );
+        nav.children("div:first-child").css(
+            "transform",
+            "translateX(-50%)"
+        ).css(
+            "position",
+            "absolute"
+        ).css(
+            "left",
+            "50%"
+        ).css(
+            "height",
+            "100%"
+        ).children("p").css(
+            "position",
+            "absolute"
+        ).css(
+            "width",
+            "400px"
+        ).css(
+            "bottom",
+            "11px"
+        ).css(
+            "font-size",
+            "56px"
+        ).css(
+            "text-align",
+            "center"
+        ).css(
+            "margin",
+            "0"
+        );
+        nav.children("div:first-child").children("img").css(
+            "position",
+            "absolute"
+        ).css(
+            "top",
+            "11px"
+        );
+        nav.find("img").css(
+            "width",
+            "128"
+        ).css(
+            "height",
+            "128"
+        );
+        $("main").css(
+            "transition",
+            "all 500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+        ).css(
+            "opacity",
+            "0"
+        ).css(
+            "margin-top",
+            "210px"
+        );
+        nav.css(
+            "transition",
+            "height 500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+        );
+        nav.css(
+            "height",
+            "210px"
+        );
     }
 });
