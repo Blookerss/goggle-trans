09a2f8db9e7d2b5d06e099eb119f4dfc5467935c
Blookers <blookyl305@gmail.com>
2021-08-29 14:09:03 +1000

    first commit

---
+++ src-tauri/src/js/index.js
@@ -0,0 +1,91 @@
+import $ from "../uiblox/jquery.module.js";
+import ky from "./ky.min.js";
+
+Array.prototype.random = function() {
+    return this[Math.floor((Math.random() * this.length))];
+};
+let languages = [
+    'en',
+    'nl',
+    'fr',
+    'tr',
+    'zu',
+    'zh-CN',
+    'ja',
+    'cs',
+    'hi'
+];
+
+const Tauri = window.__TAURI__;
+let currentInput, currentOutput;
+$(window).on("load", async() => {
+    await ky.get("https://goggletrans.blookers.repl.co/api");
+    const embedButton = $("#embedButton");
+    const transOutput = $(".trans-output");
+    const transButton = $("#translateButton");
+    const transInput = $("#translationInput");
+    const transCount = $(".translateCount");
+    const amountInd = $("#amountInd");
+    transButton.on("click", async() => {
+        embedButton.prop("disabled", true);
+        transButton.prop("disabled", true);
+        transInput.prop("disabled", true);
+        transOutput.html("Translating...");
+
+        try {
+            let input = transInput.val();
+            let { result } = await ky.post("https://goggletrans.blookers.repl.co/api/translate", {
+                json: {
+                    input,
+                    language: 'en',
+                    translateTimes: parseInt(transCount.val()),
+                    outputLanguage: 'auto'
+                }
+            }).json();
+            currentInput = input;
+            currentOutput = result;
+            transOutput.html(result);
+        } catch (err) {
+            transOutput.html(`Translation Failed!<br>${err}`);
+        }
+
+        embedButton.prop("disabled", false);
+        transButton.prop("disabled", false);
+        transInput.prop("disabled", false);
+    });
+    embedButton.on('click', async() => {
+        embedButton.prop("disabled", true);
+        let { id } = await ky.post("https://goggletrans.blookers.repl.co/embed", {
+            json: {
+                input: currentInput,
+                output: currentOutput
+            }
+        }).json();
+        await Tauri.clipboard.writeText(`https://goggletrans.blookers.repl.co/embed/${id}`);
+        embedButton.children(".btn-label").html("Copied Link!");
+        setTimeout(() => {
+            embedButton.children(".btn-label").html("Embed");
+        }, 1500);
+        embedButton.prop("disabled", false);
+    });
+    transInput.on('input change', async({ target }) => {
+        transButton.prop("disabled", !$(target).val().length);
+    });
+    transCount.on('input change', async({ target }) => {
+        amountInd.html(`Amount: ${$(target).val()}`);
+    });
+
+    $("#min-win").on('click', (event) => {
+        Tauri.window.getCurrent().minimize();
+    });
+    $("#max-win").on('click', async(event) => {
+        const window = Tauri.window.getCurrent();
+        if (await window.isMaximized())
+            window.unmaximize();
+        else
+            window.maximize();
+    });
+    $("#exit-win").on('click', (event) => {
+        Tauri.window.getCurrent().close();
+    });
+});
