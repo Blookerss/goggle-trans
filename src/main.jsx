4464f8d155eaa38dfdc624dce2ff56143921d763
Blookers <blookyl305@gmail.com>
2022-03-30 13:01:08 +1000

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
 
-import DataController from "./dataController";
-DataController.build().then(dataController => {
-    window.$__DATA__ = dataController;
+import { Home, History, Settings, NotFound } from './pages';
 
-    const { Home, History, Settings, Generator, NotFound } = require('./pages');
+const Tauri = window.__TAURI__;
+const rootElement = document.getElementById('root');
 
-    const Tauri = window.__TAURI__;
-    const rootElement = document.getElementById('root');
-    ReactDOM.render(
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/history" element={<History />} />
-                <Route exact path="/settings" element={<Settings />} />
-                <Route exact path="/generator" element={<Generator />} />
-                <Route path="/*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>,
-        rootElement
-    );
+import '/voxeliface/src/index.css';
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/history" element={<History/>}/>
+            <Route exact path="/settings" element={<Settings/>}/>
+            <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>,
+    rootElement
+);
 
-    if (Tauri) {
-        rootElement.classList.add("root-tauri");
-        document.body.classList.add("body-tauri");
-    }
-
-    serviceWorker.unregister();
-});
+if (Tauri) {
+    rootElement.classList.add("root-tauri");
+    document.body.classList.add("body-tauri");
+}
