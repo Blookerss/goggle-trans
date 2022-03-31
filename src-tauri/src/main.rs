00fdcb48d33b8437c55d764580c7c4c69c9a3022
Blookers <blookyl305@gmail.com>
2022-03-31 16:18:55 +1000

    add rust fs commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,10 @@
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
+            fs_read_text_file,
+            fs_create_dir_all,
+            fs_file_exists,
+            fs_write_file,
             web_request,
             translate
         ])
@@ -63,4 +67,33 @@ async fn translate(body: Body) -> Result<ResponseData, String> {
         }
     }
     return Err("".into());
+}
+
+#[tauri::command]
+fn fs_read_text_file(path: String) -> Result<String, String> {
+    let result = std::fs::read_to_string(path);
+    if result.is_ok() {
+        return Ok(result.unwrap());
+    }
+    return Err(result.unwrap_err().to_string());
+}
+
+#[tauri::command]
+fn fs_create_dir_all(path: String) {
+    std::fs::create_dir_all(path).ok();
+}
+
+#[tauri::command]
+fn fs_file_exists(path: String) -> bool {
+    return std::path::Path::new(&*path).exists();
+}
+
+#[tauri::command]
+fn fs_write_file(path: String, contents: String) -> Result<(), String> {
+    std::fs::create_dir_all(std::path::Path::new(&path).parent().unwrap()).unwrap();
+    let result = std::fs::write(path, contents);
+    if result.is_ok() {
+        return Ok(result.unwrap());
+    }
+    return Err(result.unwrap_err().to_string());
 }
