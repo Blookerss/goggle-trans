6edeaa723ad4d4a505dad9920a4991dd160352b9
Blookers <blookyl305@gmail.com>
2022-04-12 19:52:21 +1000

    Update Components

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,74 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Grid from '/voxeliface/components/Grid';
+import Divider from '/voxeliface/components/Divider';
 import Typography from '/voxeliface/components/Typography';
 
-export default class Accordion extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false
-        };
-    }
-
-    render() {
-        const { color } = this.props;
-        return (
-            <Grid
-                direction="vertical"
-                {...this.props}
-                css={{
-                    background: color ?? '#2C2C2C',
-
-                    '> *': {
-                        '&:first-child': {
-                            borderTopLeftRadius: 4,
-                            borderTopRightRadius: 4
-                        },
-
-                        '&:last-child': {
-                            borderBottomLeftRadius: 4,
-                            borderBottomRightRadius: 4
-                        }
-                    },
-                    ...this.props.css
-                }}
-            >
-                <Grid onClick={this.onClick.bind(this)} alignItems="center" css={{
-                    cursor: 'pointer',
-                    padding: '0 24px',
-                    minHeight: this.state.open ? 64 : 48,
-                    transition: 'min-height 214ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-                    backgroundColor: color ?? '#2C2C2C'
-                }}>
-                    {this.props.summary ??
-                        <Typography
-                            text={this.props.title ?? "Accordion Title"}
-                            size={this.props.titleSize ?? "1.2rem"}
-                            color={this.props.titleColor ?? "#ffffffd9"}
-                            weight={600}
-                            lineheight={1.5}
-                        />
-                    }
-                </Grid>
-                <Grid direction="vertical" css={{
-                    height: this.state.open ? 'auto' : 0,
-                    padding: this.state.open ? '12px 24px 24px' : '0 24px',
-                    overflow: 'hidden',
-                    background: color ?? '#2C2C2C',
-                    visibility: this.state.open ? 'visible' : 'hidden',
-                    transition: 'all 214ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'
-                }}>
-                    {this.props.children}
-                </Grid>
+export default function Accordion({ css, color, title, summary, children, titleSize, titleColor, ...props }) {
+    const [open, setOpen] = useState(false);
+    const toggle = () => setOpen(!open);
+    return (
+        <Grid
+            direction="vertical"
+            borderRadius="8px"
+            {...props}
+            css={{
+                overflow: 'hidden',
+                background: color ?? '#2C2C2C',
+                ...css
+            }}
+        >
+            <Grid onClick={toggle} alignItems="center" css={{
+                cursor: 'pointer',
+                padding: '0 24px',
+                minHeight: open ? 64 : 48,
+                userSelect: 'none',
+                transition: 'min-height 214ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+                backgroundColor: color ?? '#2C2C2C'
+            }}>
+                {summary ??
+                    <Typography
+                        text={title ?? "Accordion Title"}
+                        size={titleSize ?? "1rem"}
+                        color={titleColor ?? "#ffffff"}
+                        weight={600}
+                        family="Nunito"
+                        lineheight={1.5}
+                    />
+                }
             </Grid>
-        );
-    }
-
-    onClick() {
-        console.log('accordion onclick');
-        this.setState({
-            open: this.state.open ? false : true
-        });
-    }
+            {open && <Divider width="100%" margin="0 0 8px 0"/>}
+            <Grid direction="vertical" css={{
+                height: open ? 'auto' : 0,
+                padding: open ? '12px 24px 24px' : '0 24px',
+                overflow: 'hidden',
+                background: color ?? '#2C2C2C',
+                visibility: open ? 'visible' : 'hidden',
+                transition: 'all 214ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'
+            }}>
+                {children}
+            </Grid>
+        </Grid>
+    );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,13 +12,19 @@ export default class Navigation extends React.Component {
                 zIndex: 1100,
                 padding: '0 24px',
                 position: 'sticky',
-                minHeight: 48,
+                minHeight: 32,
                 background: '#181818'
             }}>
                 <Grid spacing="24px" direction="horizontal" alignItems="center">
-                    <Typography text={this.props.title} color="#cbcbcb"/>
+                    <Typography size=".85rem" text={this.props.title} color="#cbcbcb" family="Nunito"/>
                 </Grid>
-                <Grid spacing="24px" direction="horizontalReverse" alignItems="center">
+                <Grid spacing="24px" direction="horizontalReverse" alignItems="center" css={{
+                    '& *': {
+                        color: '#c7c6c6',
+                        fontSize: '.8rem',
+                        fontFamily: 'Nunito'
+                    }
+                }}>
                     {this.props.children}
                 </Grid>
             </Grid>
