f141013741ca4550d0ca56ca2436f06d2712a9bb
Blookers <blookyl305@gmail.com>
2022-04-12 19:51:54 +1000

    Improve Settings User Interface

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -8,46 +8,59 @@ import Grid from '/voxeliface/components/Grid';
 import Button from '/voxeliface/components/Button';
 import Header from '../components/Header';
 import Toggle from '../components/Toggle';
+import Divider from '/voxeliface/components/Divider';
 import Navigation from '../components/Navigation';
 import Typography from '/voxeliface/components/Typography';
 
 import DataController from '/src/common/dataController';
 const SettingsData = await DataController.getData("settings");
 
-export default class SettingsPage extends React.Component {
-    render() {
-        return (
-            <App>
-                <Header/>
-                <Navigation title="Settings">
-                    <Link to="/" text="Home" icon={<HouseDoorFill/>} color="#c7c6c6"/>
-                </Navigation>
-                <Main>
-                    <Grid width="100%" spacing="64px" padding="0 0 64px 0" direction="vertical">
-                        <Grid spacing="4px" direction="vertical">
-                            <Typography text="Translation History" size="1.2rem"/>
-                            <Typography text="This will determine wether translations can be saved." size="0.9rem" color="#cbcbcb"/>
-                            <Typography text="Disabling history will not delete your existing history." size="0.9rem" color="#cbcbcb" margin="0 0 8px 0"/>
+export default function SettingsPage() {
+    const settingChanged = (path, newValue) => SettingsData.set(path, newValue);
+    return (
+        <App>
+            <Header/>
+            <Navigation title="Settings">
+                <Link to="/" text="Home" icon={<HouseDoorFill/>}/>
+            </Navigation>
+            <Main>
+                <Grid width="100%" padding="0 0 64px 0" direction="vertical">
+                    <Grid width="100%">
+                        <Grid spacing="2px" direction="vertical" css={{ flexGrow: 1 }}>
+                            <Typography size="1rem" family="Nunito">
+                                Translation History
+                            </Typography>
+                            <Typography size=".9rem" color="#cbcbcb" family="Nunito">
+                                This will determine whether translations can be saved.<br/>
+                                Disabling history will not delete your existing history.
+                            </Typography>
+                        </Grid>
+                        <Grid direction="vertical" justifyContent="center">
                             <Toggle
+                                size="small"
                                 checked={SettingsData.get("history.enabled")}
-                                changed={v => this.settingChanged("history.enabled", v)}
+                                changed={v => settingChanged("history.enabled", v)}
                             />
                         </Grid>
-                        <Grid spacing="4px" direction="vertical">
-                            <Typography text="Erase Data" size="1.2rem"/>
-                            <Typography text="Erasing your data will delete all saved data," size="0.9rem" color="#cbcbcb"/>
-                            <Typography text="including history and other settings." size="0.9rem" color="#cbcbcb" margin="0 0 8px 0"/>
+                    </Grid>
+                    <Divider width="100%" margin="1rem 0"/>
+                    <Grid width="100%">
+                        <Grid spacing="2px" direction="vertical" css={{ flexGrow: 1 }}>
+                            <Typography size="1rem" family="Nunito">
+                                Erase Data
+                            </Typography>
+                            <Typography size=".9rem" color="#cbcbcb" family="Nunito">
+                                Erasing your data will delete all saved data, including history and other settings.
+                            </Typography>
+                        </Grid>
+                        <Grid direction="vertical" justifyContent="center">
                             <Button>
                                 Erase All Data
                             </Button>
                         </Grid>
                     </Grid>
-                </Main>
-            </App>
-        );
-    }
-
-    settingChanged(path, newValue) {
-        SettingsData.set(path, newValue);
-    }
+                </Grid>
+            </Main>
+        </App>
+    );
 };
